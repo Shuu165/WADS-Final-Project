08cefedc9da573d91ebfdd91e1574e46af234bb4
Shuu165 <bluestrikesg@gmail.com>
2026-06-16 11:19:52 +0700

    fix: update session cookie settings for production

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -19,6 +19,8 @@ export async function POST(req: NextRequest) {
     maxAge: expiresIn,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    path: "/",
   });
 
   return NextResponse.json({ status: "success" });
